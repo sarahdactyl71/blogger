ffd52e0329ba43ebfefaa53ab5fa94baa6797cfa
Sarah Kirk <kirk710@gmail.com>
2017-05-14 22:55:05 -0600

    adds flash messages to the create, destroy, and update methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,11 +17,15 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     @article.save
 
+    flash.notice = "Article '#{@article.title}' Created!"
+
     redirect_to article_path(@article)
   end
 
   def destroy
     @article.destroy
+
+    flash.notice = "Article '#{@article.title}' Deleted!"
 
     redirect_to articles_path
   end
