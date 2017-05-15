4fa64927e60e20377475ae65b4c57df474b52106
Sarah Kirk <kirk710@gmail.com>
2017-05-14 22:49:34 -0600

    adds the functionality for update edit and delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:destroy, :edit, :update]
 
   def index
     @articles = Article.all
@@ -19,10 +20,32 @@ class ArticlesController < ApplicationController
     redirect_to article_path(@article)
   end
 
+  def destroy
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
+  def update
+    @article.update(article_params)
+
+    flash.notice = "Article '#{@article.title}' Updated!"
+
+    redirect_to article_path(@article)
+  end
+
+  def edit
+
+  end
+
   private
 
   def article_params
     params.require(:article).permit(:title, :body)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 
 end
